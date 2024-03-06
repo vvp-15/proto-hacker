e3e4b5c11a77cd801e72f790f21edd0bb655c39d
vvp15 <vaibhavp1501@gmail.com>
2024-03-07 00:33:36 +0530

    few changes

--- primeTime/main.go
+++ primeTime/main.go
@@ -35,9 +35,11 @@ func main() {
 			return
 		}
 		cnt++
+		fmt.Println("New Remote Connection ", conn.RemoteAddr().String())
 		go handleConnection(conn, cnt)
 	}
 }
+
 func handleConnection(conn net.Conn, cnt int) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
@@ -51,9 +53,9 @@ func handleConnection(conn net.Conn, cnt int) {
 			return
 		}
 
-		if  buffer[n - 1] != '\n' {
-		//    conn.Write([]byte("malformed"))
-		   continue
+		if buffer[n-1] != '\n' {
+			//    conn.Write([]byte("malformed"))
+			continue
 		}
 
 		fmt.Printf("Received data => :%d -> %s\n", cnt, buffer[:n-1])
